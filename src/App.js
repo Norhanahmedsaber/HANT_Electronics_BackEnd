931bda8cacddb7627e40b32e9cec2d39633c2507
anas12312 <anashesham000@gmail.com>
2023-05-14 15:41:17 +0300

    'anas'

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post("/signIn", (req,res)=>{
+app.post("/signin", (req,res)=>{
     console.log(req.body.username);
-    console.log(req.body.password)
-    res.status(200).send();
+    console.log(req.body.password);
+    res.status(200).send("Done");
 })
-
+app.post("/signup", (req,res)=>{
+    console.log(req.body)
+    res.send({Data:"Done"});
+})
 app.listen(port, () => {
     console.log('Server is Running on port: ' + port)
 })
