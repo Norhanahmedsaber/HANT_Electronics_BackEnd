d8599aaa5bcbcc847e7bb6e53bfbbba1e2a07441
Norhanahmedsaber <norhan3182001@gmail.com>
2023-05-14 16:23:46 +0300

    'noor'

--- src/App.js
+++ src/App.js
@@ -3,22 +3,28 @@ const users = [{
     password: "123"
 }]
 const express = require('express')
-
-
 const app = express()
 const port = process.env.PORT || 3000
-
 app.use(express.json())
-
+const findUser = (username, password)=> {
+    const obj = users.find((user)=>{
+        return user.username === username && user.password === password
+    })
+    console.log(obj)
+    if(obj) {
+        return true;
+    }
+    return false;
+}
 app.post("/signIn", (req,res)=>{
-    console.log(req.body);
-    res.status(200).send({
-        message:"ali"
-    });
-
-app.post("/signup", (req,res)=>{
+    const username = req.body.username
+    const password = req.body.password
     console.log(req.body)
-    res.send({Data:"Done"});
+    if(findUser(username, password)) {
+        res.status(200).send({message:"Logged In"})
+    }else {
+        res.status(400).send({message: "Username or Password Incorrect"})
+    }
 
 })
 app.listen(port, () => {
