9b0a75e81af9f2335496cae51a054a10274d2a17
anas12312 <anashesham000@gmail.com>
2023-05-14 11:03:16 +0300

    anas

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 const express = require('express')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const unitRouter = require('./routers/unit')
-const nodeRouter = require('./routers/node')
-const accountRouter = require('./routers/account')
-const resourceRouter = require('./routers/rescource')
+
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.use(accountRouter)
-app.use(userRouter)
-app.use(unitRouter)
-app.use(nodeRouter)
 
 app.listen(port, () => {
     console.log('Server is Running on port: ' + port)
