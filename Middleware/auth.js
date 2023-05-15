eb4898e47d0eccdbca072d080d6e47b5e8698fd8
2152001 <tokaabdelgawad60@gmail.com>
2023-05-15 18:11:23 +0300

    'toka'

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -4,11 +4,9 @@ const User = require('../Models/User');
 const auth = async (req , res , next )=>
 {
     try {
-        console.log("hahah")
         const token = req.header('Authorization').replace('Bearer ', '');
         console.log(token)
         const decoded = jwt.verify(token,'HANT')
-        console.log("sayed3")
         const user = await User.getById(decoded.id)
         if(!user) throw new Error()
 
@@ -18,7 +16,7 @@ const auth = async (req , res , next )=>
 
     }catch(error)
     {
-        console.error(error)
+        
         res.status(401).send({error :"user not authorized"})
     }
 
