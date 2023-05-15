d0ed00c787eaf6cc4e6b0efd1ede74cd655fa3e0
hussien-mostafa <hussienmostafa888@gmail.com>
2023-05-15 16:12:10 +0300

    'hussien'

--- DB/Postgres.js
+++ DB/Postgres.js
@@ -1,11 +1,11 @@
-const pg = require('pg');
+const pg = require("pg");
 
 const pool = new pg.Pool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: "me",
-    password: '0102736245',
-    database: "test"
-})
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: "node",
+  password: "1234",
+  database: "test",
+});
 
-module.exports = pool
+module.exports = pool;
--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
+const express = require("express");
+const userRouter = require("../Routers/User");
+const listRouter = require("../Routers/List");
+const componentRouter = require("../Routers/Component");
+const phonesRouters = require("../Routers/Phones");
+const storesRouter = require("../Routers/Store");
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.json());
 
-const express = require('express')
-const userRouter = require('../Routers/User')
-const listRouter = require('../Routers/List')
-const componentRouter = require('../Routers/Component')
-const app = express()
-const port = process.env.PORT || 3000
-app.use(express.json())
-
-
-
-app.use(userRouter)
-app.use(listRouter)
-app.use(componentRouter)
-
-
+app.use(userRouter);
+app.use(listRouter);
+app.use(componentRouter);
+app.use(phonesRouters);
+app.use(storesRouter);
 
 app.listen(port, () => {
-    console.log('Server is Running on port: ' + port)
-})
+  console.log("Server is Running on port: " + port);
+});
