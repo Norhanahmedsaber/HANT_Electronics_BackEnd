5d7fa2efde88116df91705445b647c57780bff0b
2152001 <tokaabdelgawad60@gmail.com>
2023-05-15 16:10:53 +0300

    'toka'

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ const express = require('express')
 const userRouter = require('../Routers/User')
 const listRouter = require('../Routers/List')
 const itemRouter = require('../Routers/Component')
+const roleRouter = require('../Routers/Role')
 const app = express()
 const port = process.env.PORT || 3000
 app.use(express.json())
@@ -12,7 +13,7 @@ app.use(express.json())
 app.use(userRouter)
 app.use(listRouter)
 app.use(itemRouter)
-
+app.use(roleRouter)
 
 
 app.listen(port, () => {
